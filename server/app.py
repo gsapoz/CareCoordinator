be3649c7c8ee19ea61141a16db5f2c9b9b91c430
Gary Sapozhnikov <gsapoz@users.noreply.github.com>
2025-08-29 19:55:26 -0400

    added TanStack query for react

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from server.db import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,3 +13,11 @@ app = FastAPI(lifespan=lifespan) #on_startup: init_db()
 @app.get("/")
 def root():
     return {"message": "Your API is now live"}
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["http://localhost:5173"], 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
